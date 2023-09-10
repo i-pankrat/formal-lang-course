3622ea60e07ad28b46631a500089d4e4779f4fe0
Ilya Pankratov <i.pankratov.main@gmail.com>
2023-09-10 16:03:27 +0300

    Add labeled_two_cycles_graph_to_dot function to graphs_lib module
    Signed-off-by: Ilya Pankratov <i.pankratov.main@gmail.com>

--- project/graphs_lib.py
+++ project/graphs_lib.py
@@ -2,12 +2,11 @@ from typing import Tuple
 
 import cfpq_data
 import networkx as nx
-import pydot
 
 LABEL = "label"
 
 
-def get_graph_info(graph_name: str) -> (int, int, set):
+def get_graph_info(graph_name: str) -> Tuple[int, int, set]:
     """Get general information about the graph.
 
     Parameters
@@ -31,3 +30,29 @@ def get_graph_info(graph_name: str) -> (int, int, set):
         labels_set.add(label)
 
     return g.number_of_nodes(), g.number_of_edges(), labels_set
+
+
+def labeled_two_cycles_graph_to_dot(
+    file_name: str, first_cycle_len: int, second_cycle_len: int, labels: Tuple[str, str]
+) -> None:
+    """Create a graph with two cycles connected by one node
+    with labeled edges and save it to dot file.
+
+    Parameters
+    ----------
+    file_name : str
+        The name to the file where the graph will be written.
+    first_cycle_len: int
+        The length of the first cycle
+    second_cycle_len: int
+        The length of the second cycle
+    labels: Tuple[str, str]
+        Labels for the first cycle edges and for the second ones
+    """
+
+    graph = cfpq_data.labeled_two_cycles_graph(
+        first_cycle_len, second_cycle_len, labels=labels
+    )
+    pydot_graph = nx.nx_pydot.to_pydot(graph)
+    pydot_graph.write_dot(file_name + ".dot")
+    return

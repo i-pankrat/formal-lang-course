068fa290883724765f9a5bf44f4ff35cf04dc5bd
Ilya Pankratov <i.pankratov.main@gmail.com>
2023-11-02 09:25:53 +0300

    Add opportunity to create ecfg from file and text
    Signed-off-by: Ilya Pankratov <i.pankratov.main@gmail.com>

--- project/ecfg.py
+++ project/ecfg.py
@@ -31,3 +31,31 @@ class ECFG:
                 productions[prod.head] = regex_body
 
         return cls(cfg.start_symbol, cfg.terminals, cfg.variables, productions)
+
+    @classmethod
+    def from_text(cls, x: str, start: any = Variable("S")):
+        productions = {}
+        ecfg_rules = [rule for rule in x.split("\n") if not rule]
+        for rule in ecfg_rules:
+            separated_rule = rule.split("->")
+            if len(separated_rule) != 2:
+                raise SyntaxError(
+                    "Invalid ecfg grammar: incorrect rule format, expected '->'"
+                )
+
+            separated_rule[0] = separated_rule[0].strip()
+            separated_rule[1] = separated_rule[1].strip()
+
+            if separated_rule[0] in productions:
+                raise SyntaxError(
+                    "Invalid ecfg grammar: there is more than one rule for one variable"
+                )
+
+            productions[Variable(separated_rule[0])] = Regex(separated_rule[1])
+
+        return ECFG(start, productions)
+
+    @classmethod
+    def from_file(cls, file_name: str, start: any = Variable("S")):
+        with open(file_name) as file:
+            return ECFG.from_text(file.read(), start)

350118cd086cebb34d11060b82971767118cb410
Ilya Pankratov <i.pankratov.main@gmail.com>
2023-11-12 13:41:33 +0300

    Create util to generate correct path for test files
    Signed-off-by: Ilya Pankratov <i.pankratov.main@gmail.com>

---
+++ tests/test_utils/manage_path.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+
+FILES_FOLDER = Path("static")
+
+
+class UnsupportedFolderForLaunchTest(Exception):
+    # Raised when test is launched from wrong folder
+    pass
+
+
+def generate_right_path_to_test_file(filename: str) -> Path:
+    relative_path = FILES_FOLDER.joinpath(filename)
+    cwd = Path.cwd()
+    if cwd.match("*/tests"):
+        return cwd.joinpath(relative_path)
+    elif cwd.match("*/formal-lang-course"):
+        return cwd.joinpath("tests").joinpath(relative_path)
+    elif cwd.match("*/project"):
+        return cwd.parent.joinpath("tests").joinpath(relative_path)
+
+    raise UnsupportedFolderForLaunchTest
